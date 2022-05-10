3259ed9290c9093037b19660fc7ee3e71765fadb
unishift <unishifft@gmail.com>
2022-05-10 15:31:50 +0300

    Improve MLflow logging

--- edge_similarity/train.py
+++ edge_similarity/train.py
@@ -1,6 +1,5 @@
 from argparse import ArgumentParser
 from pathlib import Path
-from urllib.parse import urlparse
 
 import mlflow
 import mlflow.pytorch
@@ -28,35 +27,16 @@ def parse_args():
 def main():
     args = parse_args()
 
+    mlflow.pytorch.autolog()
+
     datamodule = SymbolDataModule(args.dataset_path)
     logger = TensorBoardLogger(args.logdir, name="EdgeMetric")
     trainer = pl.Trainer(logger=logger, max_epochs=args.epochs, gpus=1)
 
-    with mlflow.start_run():
-        mlflow.log_params({
-            'lr': args.lr,
-            'backbone': args.backbone,
-            'epochs': args.epochs
-        })
+    model = EdgeMetric(args.backbone, args.lr)
+    trainer.fit(model, datamodule=datamodule)
 
-        model = EdgeMetric(args.backbone, args.lr)
-        trainer.fit(model, datamodule=datamodule)
-
-        metrics = trainer.validate(model, datamodule=datamodule)
-        mlflow.log_metrics(metrics[0])
-
-        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
-
-        # Model registry does not work with file store
-        if tracking_url_type_store != "file":
-
-            # Register the model
-            # There are other ways to use the Model Registry, which depends on the use case,
-            # please refer to the doc for more information:
-            # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-            mlflow.pytorch.log_model(model, "model", registered_model_name="EdgeMetric")
-        else:
-            mlflow.pytorch.log_model(model, "model")
+    metrics = trainer.validate(model, datamodule=datamodule)
 
 
 if __name__ == '__main__':
